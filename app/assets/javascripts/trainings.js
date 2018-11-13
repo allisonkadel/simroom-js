9913a546204bd7787b92fc3ecdeedf60d6e54407
allisonkadel <owner@allisonkadel.com>
2018-11-13 09:10:01 -0800

    create ajax get request on 'create new training' to render new training form on training index page

--- app/assets/javascripts/trainings.js
+++ app/assets/javascripts/trainings.js
@@ -1,3 +1,11 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+$(function() {
+
+    $("a.new_training").on('click',function(e) {
+        e.preventDefault();
+
+        $.get(this.href).success(function(response) {
+            $("div.training_form").html(response)
+        });
+    });
+
+});
