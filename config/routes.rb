c1500a78b9569cfb39208658c8ff60e84553c1b7
allisonkadel <owner@allisonkadel.com>
2018-11-16 07:48:29 -0800

    create route for trainings next action

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,7 @@ Rails.application.routes.draw do
 
   get '/auth/:provider/callback', to: 'sessions#create'
 
+  get '/trainings/:id/next', to: 'trainings#next'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
